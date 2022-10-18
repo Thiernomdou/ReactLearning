ed7b4667e0e24f6ba01c3053e7384683a22c41ca
DIALLO Thierno <thiernomdou63@gmail.com>
2022-10-18 21:37:47 +0200

    fonctions_flechées

--- RappelJSModerne/app.js
+++ RappelJSModerne/app.js
@@ -1,7 +1,26 @@
 //scope
-function foo() {
-    var x = 5;
-    console.log(x);
-}
+// function foo() {
+//     var x = 5;
+//     console.log(x);
+// }
 
-foo();
+// foo();
+
+//fleché
+// const add = (a,b) => {
+//     return a + b;
+// }
+// console.log(add(2,2));
+
+//mot clé this
+//this avec les fonctions flechés fait réference au contexte englobant (window en l'ocurence)
+//this sans les fonctions flechées fait réference à l'objet appelant (myObj en l'occurence)
+// const myObj = {
+//     a: "5",
+//     foo: () => {
+//         console.log(this.foo);
+//     }
+// }
+// myObj.foo();
+
+//spread & Rest perator
