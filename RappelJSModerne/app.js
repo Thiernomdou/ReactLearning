f1f922f0a46861458d1f41a05fb842f11aa8afd7
DIALLO Thierno <thiernomdou63@gmail.com>
2022-10-19 23:08:41 +0200

    spread & Rest perator

--- RappelJSModerne/app.js
+++ RappelJSModerne/app.js
@@ -24,3 +24,31 @@
 // myObj.foo();
 
 //spread & Rest perator
+//Spread Operator
+// const arr = [1,2,3];
+// const arr2 = [...arr,4]
+// console.log(...arr2);
+
+// const myObj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// const myObj2 = {
+//     ...myObj,
+//     d:4
+// }
+// console.log(myObj2);
+
+//Rest Operator (tous les arguments qui reste qu'on récupère sous un tableau)
+// const add = (...args) => {
+//     let result = 0;
+//     //for of pour les tableaux(notament ici) et for in pour les objets
+//     for(const arg of args) {
+//         result += arg;
+//     }
+//     return result;
+// }
+// console.log(add(2,2,6,8));
+
+//Les fonction pures
