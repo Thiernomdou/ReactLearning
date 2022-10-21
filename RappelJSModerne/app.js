c898ed239fe30813f6af212a4dad72cc757cb39d
DIALLO Thierno <thiernomdou63@gmail.com>
2022-10-21 21:33:35 +0200

    Les fonctions d'ordre supérieur des tableaux

--- RappelJSModerne/app.js
+++ RappelJSModerne/app.js
@@ -104,3 +104,13 @@ retourne une autre, ou les deux*/
 // console.log(arrSup100);
 // console.log(arrSup200);
 
+
+//Les fonctions d'ordre supérieur des tableaux
+const arr = [1,2,3,4,5,6];
+//la méthode map retourn e un nouveau tableau
+const mapedArr = arr.map(x => x + 10);
+console.log(mapedArr);
+//La méthode Filter retourne un nouveau tableau
+const filteredArr = arr.filter(num => num > 2);
+console.log(filteredArr);
+
